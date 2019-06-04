6740a85e501de12e499e334dace3d4aefe621f40
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-06-03 22:37:16 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+const { app, BrowserWindow, Menu } = require('electron')
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 600, 
+    height: 600,
+    icon: `file://${__dirname}/dist/assets/logo.png`
+  })
+  const template = [
+    ...(process.platform === 'darwin' ? [{
+      label: app.getName(),
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
+    {
+      label: 'File',
+      submenu: [
+        process.platform === 'darwin' ? { role: 'close' } : { role: 'quit' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternalSync('https://electronjs.org') }
+        }
+      ]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  win.loadURL(`file://${__dirname}/dist/desktop-chime-timer/index.html`)
+
+
+
+  // Event when the window is closed.
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+// Create window on electron intialization
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desktop-chime-timer';
+
+  max     = 1;
+  current = 0;
+
+  /// Start the timer
+  start() {
+    const myInterval = interval(100);
+    
+        myInterval.subscribe(i => this.current += 0.1);
+          /*.takeWhile(_ => !this.isFinished )
+          .do(i => this.current += 0.1)
+          .subscribe();*/
+  }
+
+   /// finish timer
+  finish() {
+    this.current = this.max;
+  }
+
+  /// reset timer
+  reset() {
+    this.current = 0;
+  }
+
+  /// Getters to prevent NaN errors
+
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return this.currentVal >= this.maxVal;
+  }
+
 }
